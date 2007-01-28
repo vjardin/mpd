43a937b59eaddaf11531f631af5fd39c118af238
amotin <amotin>
2007-01-28 14:19:19 +0000

    add some safety to RAD_MICROSOFT_MS_CHAP_ERROR

--- src/radius.c
+++ src/radius.c
@@ -1272,6 +1272,13 @@ RadiusGetParams(AuthData auth, int eap_proxy)
 	    switch (res) {
 
 	      case RAD_MICROSOFT_MS_CHAP_ERROR:
+	        if (auth->mschap_error != NULL) {
+	    	    Freee(MB_AUTH, auth->mschap_error);
+		    auth->mschap_error = NULL;
+		}
+		if (len == 0)
+		    break;
+
 		/* there is a nullbyte on the first pos, don't know why */
 		if (((const char *)data)[0] == '\0') {
 		  data = (const char *)data + 1;
