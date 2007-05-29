afc3bcec3dd58c45cbc7dba2cb0b3ea66fcb7143
amotin <amotin>
2007-05-29 20:02:59 +0000

    compact radius structs

--- src/radius.h
+++ src/radius.h
@@ -83,8 +83,8 @@
   struct radiusserver_conf {
     char	*hostname;
     char	*sharedsecret;
-    int		auth_port;
-    int		acct_port;
+    in_port_t	auth_port;
+    in_port_t	acct_port;
     struct	radiusserver_conf *next;
   };
   typedef struct radiusserver_conf *RadServe_Conf;
@@ -96,7 +96,7 @@
     int 	acct_update;		/* Accounting Update Interval */
     struct	in_addr radius_me;
     struct	u_addr radius_mev6;
-    char	file[PATH_MAX];
+    char	*file;
     struct radiusserver_conf *server;
     struct optinfo	options;	/* Configured options */
   };
