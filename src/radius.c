da6b718ab4a5fd95d3cc65250ac2758a9d59d101
amotin <amotin>
2009-07-14 10:58:46 +0000

    Log proper value.

--- src/radius.c
+++ src/radius.c
@@ -1555,7 +1555,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
       case RAD_REPLY_MESSAGE:
 	tmpval = rad_cvt_string(data, len);
 	Log(LG_RADIUS2, ("[%s] RADIUS: Get RAD_REPLY_MESSAGE: %s ",
-	  auth->info.lnkname, auth->reply_message));
+	  auth->info.lnkname, tmpval));
 	auth->reply_message = Mdup(MB_AUTH, tmpval, len + 1);
 	free(tmpval);
         break;
