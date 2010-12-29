0c79ac2dbe9cb1a6e89afca65792fb735555a529
amotin <amotin>
2010-12-29 12:13:27 +0000

    Tune some log messages.

--- src/radius.c
+++ src/radius.c
@@ -810,11 +810,11 @@ RadiusStart(AuthData auth, short request_type)
 	    }
 	}
 	if (auth->params.peername[0]) {
-	    Log(LG_RADIUS2, ("[%s] RADIUS: Put Agent-Remote-Id: %s",
+	    Log(LG_RADIUS2, ("[%s] RADIUS: Put ADSL-Agent-Remote-Id: %s",
     		auth->info.lnkname, auth->params.peername));
 	    if (rad_put_vendor_string(auth->radius.handle, 3561, 2,
     		    auth->params.peername) == -1) {
-    		Log(LG_RADIUS, ("[%s] RADIUS: Put Agent-Remote-Id failed %s",
+    		Log(LG_RADIUS, ("[%s] RADIUS: Put ADSL-Agent-Remote-Id failed %s",
     	    	    auth->info.lnkname, rad_strerror(auth->radius.handle)));
     		return (RAD_NACK);
 	    }
