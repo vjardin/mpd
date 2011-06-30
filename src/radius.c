19b860bc6a357b1ab2ed8cdbbeff57bdc4554e6a
dmitryluhtionov <dmitryluhtionov>
2011-06-30 14:22:56 +0000

    Fix bug 3175664

--- src/radius.c
+++ src/radius.c
@@ -1427,7 +1427,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
       case RAD_USER_NAME:
 	tmpval = rad_cvt_string(data, len);
 	/* copy it into the persistent data struct */
-	strcpy(auth->params.authname, tmpval);
+	strlcpy(auth->params.authname, tmpval, sizeof(auth->params.authname));
 	free(tmpval);
 	Log(LG_RADIUS2, ("[%s] RADIUS: Get RAD_USER_NAME: %s ",
 	  auth->info.lnkname, auth->params.authname));
